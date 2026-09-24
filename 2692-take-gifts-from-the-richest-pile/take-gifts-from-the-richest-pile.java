c168214cdd823c458bd1a107aa711c10856c07ae
MRITHULA P <mrithulapalani06@gmail.com>
2026-09-24 19:06:13 +0530

    Time: 7 ms (64.71%) | Memory: 44.2 MB (53.88%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int gift : gifts) {
+            pq.offer(gift);
+        }
+
+        while (k-- > 0) {
+            int max = pq.poll();
+            pq.offer((int) Math.sqrt(max));
+        }
+
+        long sum = 0;
+
+        while (!pq.isEmpty()) {
+            sum += pq.poll();
+        }
+
+        return sum;
+    }
+}
